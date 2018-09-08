babfbd915db205f1f91cab9645e13a1d2dec66ca
bmeneghini <bmuschioni@gmail.com>
2018-09-07 23:03:34 -0300

    Finalização do front da tela de upload de jogos

--- src/components/meus_jogos/enviar_jogo/enviar_jogo_form.js
+++ src/components/meus_jogos/enviar_jogo/enviar_jogo_form.js
@@ -5,7 +5,13 @@ import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import Send from '@material-ui/icons/Send';
+import Image from '@material-ui/icons/Image';
+import Movie from '@material-ui/icons/Movie';
 import CustomLabel from '../../shared/custom_label';
+import FolderIcon from '@material-ui/icons/Folder';
+import Card from '@material-ui/core/Card';
+import pink from '@material-ui/core/colors/pink';
+import Avatar from '@material-ui/core/Avatar';
 
 export default class EnviarJogoForm extends Component {
 
@@ -16,7 +22,11 @@ export default class EnviarJogoForm extends Component {
             desenvolvedor: "",
             preco: "",
             genero: "",
-            imagens: {}
+            trailer: "",
+            selectedLogo: null,
+            selectedLogoName: "",
+            selectedGame: null,
+            selectedGameName: "",
         }
     }
 
@@ -26,7 +36,15 @@ export default class EnviarJogoForm extends Component {
         });
     };
 
-    onImagemChange = (imagens) => this.setState({ imagens });
+    fileChangedHandler = (event) => {
+        this.setState({ selectedLogo: event.target.files[0] })
+        if (event.target.files[0] != null) this.setState({ selectedLogoName: event.target.files[0].name })
+    }
+
+    gameChangedHandler = (event) => {
+        this.setState({ selectedGame: event.target.files[0] })
+        if (event.target.files[0] != null) this.setState({ selectedGameName: event.target.files[0].name })
+    }
 
     render() {
 
@@ -63,55 +81,55 @@ export default class EnviarJogoForm extends Component {
                 <form autoComplete="off">
                     <FormGroup>
                         <FormControl>
-                            <TextField
-                                id="name"
-                                label="Nome"
-                                value={this.state.nome}
-                                onChange={this.handleChange('nome')}
-                                margin="normal"
-                            />
+                            <div className={"form-div"}>
+                                <TextField
+                                    id="name"
+                                    label="Nome"
+                                    required
+                                    value={this.state.nome}
+                                    onChange={this.handleChange('nome')}
+                                    margin="normal"
+                                />
+                                <TextField
+                                    id="desenvolvedor"
+                                    label="Desenvolvedor"
+                                    required
+                                    value={this.state.desenvolvedor}
+                                    onChange={this.handleChange('desenvolvedor')}
+                                    margin="normal"
+                                />
+                            </div>
                         </FormControl>
                     </FormGroup>
-                    <FormGroup>
+                    <FormGroup style={{ height: 75 }}>
                         <FormControl>
-                            <TextField
-                                id="desenvolvedor"
-                                label="Desenvolvedor"
-                                value={this.state.desenvolvedor}
-                                onChange={this.handleChange('desenvolvedor')}
-                                margin="normal"
-                            />
-                        </FormControl>
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControl>
-                            <TextField
-                                id="preco"
-                                label="Preço"
-                                type="number"
-                                value={this.state.preco}
-                                onChange={this.handleChange('preco')}
-                                margin="normal"
-                            />
-                        </FormControl>
-                    </FormGroup>
-                    <FormGroup>
-                        <FormControl>
-                            <TextField
-                                id="genero"
-                                select
-                                label="Genero"
-                                value={this.state.genero}
-                                onChange={this.handleChange('genero')}
-                                helperText="Selecione um"
-                                margin="normal"
-                            >
-                                {generos.map(option => (
-                                    <MenuItem key={option.value} value={option.value}>
-                                        {option.label}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
+                            <div className={"form-div"}>
+                                <TextField
+                                    id="preco"
+                                    label="Preço"
+                                    required
+                                    type="number"
+                                    value={this.state.preco}
+                                    onChange={this.handleChange('preco')}
+                                    margin="normal"
+                                />
+                                <TextField
+                                    id="genero"
+                                    select
+                                    label="Genero"
+                                    required
+                                    value={this.state.genero}
+                                    onChange={this.handleChange('genero')}
+                                    helperText="Selecione um"
+                                    margin="normal"
+                                >
+                                    {generos.map(option => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </div>
                         </FormControl>
                     </FormGroup>
                     <FormGroup>
@@ -119,11 +137,100 @@ export default class EnviarJogoForm extends Component {
                             <TextField
                                 id="descricao"
                                 label="Descrição"
+                                required
                                 value={this.state.descricao}
                                 onChange={this.handleChange('descricao')}
                                 multiline
                                 margin="normal"
                             />
+                        </FormControl>
+                    </FormGroup>
+                    <FormGroup style={{ height: 305 }}>
+                        <FormControl>
+                            <Card className={"card-upload"}>
+                                <div>
+                                    <input
+                                        accept="image/*"
+                                        id="contained-button-file"
+                                        multiple
+                                        type="file"
+                                        style={{ display: 'none', }}
+                                        onChange={this.fileChangedHandler}
+                                    />
+                                    <label htmlFor="contained-button-file" style={{ padding: 15 }}>
+                                        <Button
+                                            style={{ width: 45, height: 45 }}
+                                            variant="fab"
+                                            color="secondary"
+                                            component="span">
+                                            <Image />
+                                        </Button>
+                                    </label>
+                                    <TextField
+                                        id="logo"
+                                        value={this.state.selectedLogoName}
+                                        required
+                                        margin="normal"
+                                        placeholder="Selecione um logo clicando no botão ao lado"
+                                        style={{ width: "70%", marginLeft: 10 }}
+                                    />
+                                </div>
+                                <div>
+                                    <input
+                                        accept="*"
+                                        id="contained-button-game-upload"
+                                        multiple
+                                        type="file"
+                                        style={{ display: 'none', }}
+                                        onChange={this.gameChangedHandler}
+                                    />
+                                    <label htmlFor="contained-button-game-upload" style={{ padding: 15 }}>
+                                        <Button
+                                            style={{ width: 45, height: 45 }}
+                                            variant="fab"
+                                            color="secondary"
+                                            component="span">
+                                            <FolderIcon />
+                                        </Button>
+                                    </label>
+                                    <TextField
+                                        id="logo"
+                                        value={this.state.selectedGameName}
+                                        required
+                                        margin="normal"
+                                        placeholder="Selecione o arquivo compactado do jogo clicando no botão ao lado"
+                                        style={{ width: "70%", marginLeft: 10 }}
+                                    />
+                                </div>
+                                <div>
+                                    <input
+                                        accept="*"
+                                        disabled 
+                                        id="contained-button-trailer"
+                                        multiple
+                                        type="file"
+                                        style={{ display: 'none', }}
+                                    />
+                                    <label htmlFor="contained-button-trailer" style={{ padding: 15 }}>
+                                        <Button
+                                            style={{ width: 45, height: 45 }}
+                                            variant="fab"
+                                            color="secondary"
+                                            component="span">
+                                            <Movie />
+                                        </Button>
+                                    </label>
+                                    <TextField
+                                        id="trailer"
+                                        value={this.state.trailer}
+                                        required
+                                        margin="normal"
+                                        onChange={this.handleChange('trailer')}
+                                        placeholder="Insira a URL do trailer"
+                                        style={{ width: "70%", marginLeft: 10 }}
+                                    />
+                                </div>
+                            </Card >
                         </FormControl>
                     </FormGroup>
                     <Button type='submit' variant="contained" color="secondary">
