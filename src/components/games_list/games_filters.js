66bb239a23b50a2692d6db64850bc9083e514c01
bmeneghini <bmuschioni@gmail.com>
2019-04-21 16:17:39 -0300

    Adding expansion panel to games filters

--- src/components/games_list/games_filters.js
+++ src/components/games_list/games_filters.js
@@ -4,6 +4,11 @@ import Button from '@material-ui/core/Button';
 import GameNameInput from './../shared/game_name_input';
 import GamePriceInput from './../shared/game_price_input';
 import GameGenderInput from './../shared/game_gender_input';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { getGamesWithFilter } from './../../actions/index';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -15,7 +20,8 @@ class GamesFilters extends React.Component {
         this.state = {
             gameName: this.props.gameName,
             gamePrice: '',
-            gameGender: ''
+            gameGender: '',
+            expanded: true
         }
     }
 
@@ -48,32 +54,46 @@ class GamesFilters extends React.Component {
         this.props.setGamesListState(result);
     }
 
+    toggleExpanded = () => {
+        this.setState({ expanded: !this.state.expanded })
+    }
+
     render() {
         return (
-            <div className={'games-filters-root'}>
-                <GameNameInput
-                    shrink={true}
-                    displaySearch={false}
-                    label={'Nome do jogo'}
-                    enableFullWidth={true}
-                    gameName={this.state.gameName}
-                    handleGameNameChange={this.handleGameNameChange}
-                />
-                <GamePriceInput
-                    label={'Preço máximo'}
-                    enableFullWidth={true}
-                    gamePrice={this.state.gamePrice}
-                    handleGamePriceChange={this.handleGamePriceChange}
-                />
-                <GameGenderInput
-                    handleGenderChange={this.handleGenderChange}
-                    gameGender={this.state.gameGender}
-                />
-                <Button onClick={this.handleFilterClick} className={'filter-button'} variant="contained" color="secondary">
-                    <FilterList className={'filter-icon'} />
-                    Filtrar
-                </Button>
-            </div >
+            <div className={'filter-games-container'}>
+                <ExpansionPanel className={'expansion-panel'} expanded={this.state.expanded}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} onClick={this.toggleExpanded}>
+                        <Typography className={'owned-games-title'}>Filtros</Typography>
+                    </ExpansionPanelSummary>
+                    <hr className={'horizontal-line'} />
+                    <ExpansionPanelDetails>
+                        <div className={'games-filters-root'}>
+                            <GameNameInput
+                                shrink={true}
+                                displaySearch={false}
+                                label={'Nome do jogo'}
+                                enableFullWidth={true}
+                                gameName={this.state.gameName}
+                                handleGameNameChange={this.handleGameNameChange}
+                            />
+                            <GamePriceInput
+                                label={'Preço máximo'}
+                                enableFullWidth={true}
+                                gamePrice={this.state.gamePrice}
+                                handleGamePriceChange={this.handleGamePriceChange}
+                            />
+                            <GameGenderInput
+                                handleGenderChange={this.handleGenderChange}
+                                gameGender={this.state.gameGender}
+                            />
+                            <Button onClick={this.handleFilterClick} className={'filter-button'} variant="contained" color="secondary">
+                                <FilterList className={'filter-icon'} />
+                                Filtrar
+                        </Button>
+                        </div >
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
         )
     }
 }
